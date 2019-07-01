5f172bf6a2471a6cc3cbc30b8fa643fd9720892b
Himanshu Tongya <Himanshu@biz4group.com>
2019-07-01 18:31:31 +0530

    Added the edit and add code

--- src/app/details-form/details-form.component.ts
+++ src/app/details-form/details-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-details-form',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-form.component.css']
 })
 export class DetailsFormComponent implements OnInit {
+  // @ViewChild('mydata') myForm: ElementRef;
   details: {id: String, name: String, dob: String, gender: String, email: String}[];
-
+  private mdlSampleIsOpen: boolean = false;
+  private modalAddUser: boolean = false;
+  index: number;
+  myForm: FormGroup;
   constructor() {
+
     // this.details = [{name: 'Dhawal Jain', dob: '29/01/1992', gender: 'M', email: 'dhawal@biz4'},]
     // this.value =
     this.details = [
@@ -19,23 +25,43 @@ export class DetailsFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      'name': new FormControl(null),
+      'dob': new FormControl(null),
+      'gender': new FormControl(null),
+      'email': new FormControl(null)
+    });
+  }
+  openModal(open: boolean, index: number) {
+    this.mdlSampleIsOpen = open;
+    console.log('index', index);
+    console.log(this.details[index]);
+    this.index = index;
   }
 
-  // employeeDetails() {
-  //   this.details[0] = {name: 'Dhawal Jain', dob: '29/01/1992', gender: 'M', email: 'dhawal@biz4'};
-  //   this.details[1] = {name: 'Dhawal Jain', dob: '29/01/1992', gender: 'M', email: 'dhawal@biz4'};
-  //   this.details[2] = {name: 'Dhawal Jain', dob: '29/01/1992', gender: 'M', email: 'dhawal@biz4'};
-  //   this.details[3] = {name: 'Dhawal Jain', dob: '29/01/1992', gender: 'M', email: 'dhawal@biz4'};
+  openModalAddUser(open: boolean) {
+    this.modalAddUser = open;
+  }
+
+  btnOKClick() {
+    this.myForm = new FormGroup({
+      'name': new FormControl(this.details[this.index].name),
+      'dob': new FormControl(this.details[this.index].dob),
+      'gender': new FormControl(this.details[this.index].gender),
+      'email': new FormControl(this.details[this.index].email)
+    });
+    console.log(this.myForm);
+    // this.form.get('first').setValue('some value');
+  // console.log(this.myForm.nativeElement.);
+  }
+  // editUser(passedId) {
+  //   // code to edit info
+  //
+  //
+  //   for (let i=0; i<this.details.length; i++ ) {
+  //     if (this.details[i].id == passedId) {
+  //       console.log(this.details[i]);
+  //     }
+  //   }
   // }
-
-  editUser(passedId) {
-    // code to edit info
-
-
-    for (var i=0; i<this.details.length; i++ ) {
-      if (this.details[i].id == passedId) {
-        console.log(this.details[i]);
-      }
-    }
-  }
 }
